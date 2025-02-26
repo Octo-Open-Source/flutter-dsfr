e6edc431687911ba635afc54022e466704f4bc7d
jasmine.lebert <jasmine.lebert@octo.com>
2025-02-26 10:39:39 +0100

    test: a11y - input

--- test/a11y_test.dart
+++ test/a11y_test.dart
@@ -5,27 +5,26 @@ import 'package:meta/meta.dart';
 import 'scaffold_for_test.dart';
 
 @isTest
-void accessibilityTest({required String componentName, required bool isLightMode, required Widget child}) {
-  testWidgets(
-    "A11y test: Composant $componentName en mode ${isLightMode ? 'clair' : 'sombre'}",
-    (WidgetTester tester) async {
-      // Given
-      final SemanticsHandle handle = tester.ensureSemantics();
+void accessibilityTest(
+    {required String componentName, required bool isLightMode, required Widget child, skip = false}) {
+  testWidgets("A11y test: Composant $componentName en mode ${isLightMode ? 'clair' : 'sombre'}",
+      (WidgetTester tester) async {
+    // Given
+    final SemanticsHandle handle = tester.ensureSemantics();
 
-      // When
-      await tester.pumpWidget(
-        ScaffoldForTest(
-          isLightMode: isLightMode,
-          child: child,
-        ),
-      );
+    // When
+    await tester.pumpWidget(
+      ScaffoldForTest(
+        isLightMode: isLightMode,
+        child: child,
+      ),
+    );
 
-      // Then
-      await expectLater(tester, meetsGuideline(androidTapTargetGuideline));
-      await expectLater(tester, meetsGuideline(labeledTapTargetGuideline));
-      await expectLater(tester, meetsGuideline(iOSTapTargetGuideline));
-      await expectLater(tester, meetsGuideline(textContrastGuideline));
-      handle.dispose();
-    },
-  );
+    // Then
+    await expectLater(tester, meetsGuideline(androidTapTargetGuideline));
+    await expectLater(tester, meetsGuideline(labeledTapTargetGuideline));
+    await expectLater(tester, meetsGuideline(iOSTapTargetGuideline));
+    await expectLater(tester, meetsGuideline(textContrastGuideline));
+    handle.dispose();
+  }, skip: skip);
 }
---
+++ test/input_a11y_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_dsfr/composants/input.dart';
+import 'a11y_test.dart';
+
+void main() {
+  Widget input = DsfrInput(label: 'label', hintText: 'hint', onChanged: (final value) {});
+
+  Widget passwordInput = DsfrInput(
+    label: 'label',
+    hintText: 'hint',
+    onChanged: (final value) {},
+    isPasswordMode: true,
+  );
+
+  Widget disabledInput = DsfrInput(
+    label: 'label',
+    hintText: 'hint',
+    onChanged: (final value) {},
+    enabled: false,
+  );
+
+  accessibilityTest(componentName: 'input', isLightMode: true, child: input);
+  accessibilityTest(componentName: 'input', isLightMode: false, child: input);
+
+  accessibilityTest(componentName: 'input password', isLightMode: true, child: passwordInput);
+  accessibilityTest(componentName: 'input password', isLightMode: false, child: passwordInput);
+
+  // Ces tests ne passe pas car le contraste est pas assez élevé
+  accessibilityTest(componentName: 'input disable', isLightMode: true, child: disabledInput, skip: true);
+  accessibilityTest(componentName: 'input disable', isLightMode: false, child: disabledInput, skip: true);
+}
