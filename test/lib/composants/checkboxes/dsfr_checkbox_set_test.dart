8682f745ab93e83858e35570a0c55fa77de93411
jaayap <lebert.jasmine@gmail.com>
2025-03-14 10:23:50 +0100

    test: ajout test

---
+++ test/lib/composants/checkboxes/dsfr_checkbox_set_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_dsfr/composants/checkboxes/checkbox.dart';
+import 'package:flutter_dsfr/composants/checkboxes/dsfr_checkbox_set.dart';
+import 'package:flutter_dsfr/helpers/composant_state.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../../../helpers/a11y_test.skip.dart';
+
+void main() {
+  var dsfrCheckboxSet = DsfrCheckboxSet(
+    label: 'label',
+    description: 'description',
+    checkboxes: [
+      DsfrCheckbox.sm(
+        label: 'label',
+        value: true,
+        onChanged: (bool value) {},
+      ),
+      DsfrCheckbox.sm(
+        label: 'label',
+        value: false,
+        onChanged: (bool value) {},
+      ),
+    ],
+  );
+
+  accessibilityTest(componentName: 'Groupe de checkbox', isLightMode: true, child: dsfrCheckboxSet);
+  accessibilityTest(componentName: 'Groupe de checkbox', isLightMode: false, child: dsfrCheckboxSet);
+}
