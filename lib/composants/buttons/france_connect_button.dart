21031add6f30f7bac02ab2039301a9178fc747a0
jaayap <lebert.jasmine@gmail.com>
2025-03-17 17:49:46 +0100

    feat: a11y :  gestion du focus

--- lib/composants/buttons/france_connect_button.dart
+++ lib/composants/buttons/france_connect_button.dart
@@ -14,42 +14,7 @@ class FranceConnectButton extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       spacing: 12,
       children: [
-        IntrinsicWidth(
-          child: DecoratedBox(
-            decoration: BoxDecoration(color: DsfrColorDecisions.backgroundActionHighBlueFrance(context)),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-              child: Row(
-                spacing: 12,
-                children: [
-                  SvgPicture.asset(
-                    'packages/flutter_dsfr/assets/icons/france_connect.svg',
-                    excludeFromSemantics: true,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'S\'identifier avec',
-                        style: DsfrTextStyle.bodyMd(color: DsfrColorDecisions.textInvertedBlueFrance(context)),
-                      ),
-                      Text(
-                        'FranceConnect',
-                        style: DsfrTextStyle.bodyMdBold(color: DsfrColorDecisions.textInvertedBlueFrance(context)),
-                      ),
-                    ],
-                  ),
-                  isFranceConnectPlus
-                      ? SvgPicture.asset(
-                          'packages/flutter_dsfr/assets/icons/france_connect_plus.svg',
-                          excludeFromSemantics: true,
-                        )
-                      : SizedBox.shrink(),
-                ],
-              ),
-            ),
-          ),
-        ),
+        _FranceConnectButton(isFranceConnectPlus: isFranceConnectPlus),
         DsfrLink(
           label: 'Qu\'est-ce que France Connect${isFranceConnectPlus ? '+' : ''} ?',
           onTap: () {
@@ -60,6 +25,79 @@ class FranceConnectButton extends StatelessWidget {
           iconPosition: DsfrLinkIconPosition.end,
         ),
       ],
+    );
+  }
+}
+
+class _FranceConnectButton extends StatefulWidget {
+  const _FranceConnectButton({
+    super.key,
+    required this.isFranceConnectPlus,
+  });
+
+  final bool isFranceConnectPlus;
+
+  @override
+  State<_FranceConnectButton> createState() => _FranceConnectButtonState();
+}
+
+class _FranceConnectButtonState extends State<_FranceConnectButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Focus(
+      canRequestFocus: true,
+      child: Builder(builder: (context) {
+        final isFocused = Focus.of(context).hasFocus;
+        return DsfrFocusWidget(
+          isFocused: isFocused,
+          child: Semantics(
+            button: true,
+            child: IntrinsicWidth(
+              child: InkWell(
+                focusColor: DsfrColorDecisions.backgroundTransparent(context),
+                onTap: () {
+                  print("onTap");
+                },
+                child: DecoratedBox(
+                  decoration: BoxDecoration(color: DsfrColorDecisions.backgroundActionHighBlueFrance(context)),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                    child: Row(
+                      spacing: 12,
+                      children: [
+                        SvgPicture.asset(
+                          'packages/flutter_dsfr/assets/icons/france_connect.svg',
+                          excludeFromSemantics: true,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'S\'identifier avec',
+                              style: DsfrTextStyle.bodyMd(color: DsfrColorDecisions.textInvertedBlueFrance(context)),
+                            ),
+                            Text(
+                              'FranceConnect',
+                              style:
+                                  DsfrTextStyle.bodyMdBold(color: DsfrColorDecisions.textInvertedBlueFrance(context)),
+                            ),
+                          ],
+                        ),
+                        widget.isFranceConnectPlus
+                            ? SvgPicture.asset(
+                                'packages/flutter_dsfr/assets/icons/france_connect_plus.svg',
+                                excludeFromSemantics: true,
+                              )
+                            : SizedBox.shrink(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
