94e91d97d46a9e514915b579819c700c1992d140
Jordan Chapuy <chapuyj@gmail.com>
2025-04-07 14:39:38 +0200

    feat(colors): création d'une page de présentation

--- example/lib/colors_page.dart
+++ example/lib/colors_page.dart
@@ -10,5 +10,73 @@ class ColorsPage extends StatelessWidget {
   );
 
   @override
-  Widget build(final context) => const Text('Colors');
+  Widget build(final context) {
+    final dsfrColors = allColors();
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: dsfrColors.length,
+            itemBuilder: (final context, final index) {
+              return ColorPresentation(dsfrColor: dsfrColors[index]);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ColorPresentation extends StatelessWidget {
+  const ColorPresentation({super.key, required this.dsfrColor});
+
+  final DsfrColor dsfrColor;
+
+  @override
+  Widget build(final context) {
+    final textColor = dsfrColor.color.computeLuminance() > 0.5 ? Colors.black : Colors.white;
+    return Container(
+      color: dsfrColor.color,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                dsfrColor.name,
+                style: TextStyle(color: textColor),
+              ),
+              Text(
+                dsfrColor.hex,
+                style: TextStyle(color: textColor),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+List<DsfrColor> allColors() {
+  //TODO: fake data
+  return Colors.primaries.map((e) {
+    return DsfrColor(
+      name: "greenTilleulVerveine850Hover",
+      hex: "#12345678",
+      color: e,
+    );
+  }).toList();
+}
+
+class DsfrColor {
+  const DsfrColor({
+    required this.name,
+    required this.hex,
+    required this.color,
+  });
+
+  final String name;
+  final String hex;
+  final Color color;
 }
